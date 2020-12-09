645d0bf34eb86a31d9b5ab424d464dc5151e5a6c
Laura Skillington <lauras@hotdoc.com.au>
2020-12-10 09:40:33 +1100

    Sets up splitting of input list

--- day-2.ts
+++ day-2.ts
@@ -1,3 +1,14 @@
 import { readFileSync } from 'fs';
 
 const passwords: string[] = readFileSync('./input-files/password-policies.txt').toString('utf8').split('\n');
+
+const validPasswords: string[] = [];
+
+passwords.forEach(entry => {
+  const policy = entry.split(':')[0].split(' ')[0];
+  const letter = entry.split(':')[0].split(' ')[1];
+  const password = entry.split(':')[1].slice(1);
+
+  const min = policy.split('-')[0];
+  const max = policy.split('-')[1];
+});
