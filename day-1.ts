eb75a82ea0f16a96c3ceca50e287f483487f40f1
Laura Skillington <lauras@hotdoc.com.au>
2020-12-10 09:24:40 +1100

    Finishes challenge

--- day-1.ts
+++ day-1.ts
@@ -1,3 +1,19 @@
 import { readFileSync } from 'fs';
 
 const expenseReport: string[] = readFileSync('./input-files/expense-report.txt').toString('utf8').split('\n');
+
+let firstExpense: number;
+let secondExpense: number;
+
+expenseReport.forEach(expenseA => {
+  const expenseAInt: number = parseInt(expenseA);
+  expenseReport.forEach(expenseB => {
+    const expenseBInt: number = parseInt(expenseB);
+    if (expenseAInt + expenseBInt === 2020) {
+        firstExpense = expenseAInt;
+        secondExpense = expenseBInt;
+    };
+  });
+});
+
+console.log(firstExpense * secondExpense)
